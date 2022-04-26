2226edbb87ee51c6cbac15e0e69d47d3b9485455
Julian Rueck <Julian.Rueck@infosupport.com>
2022-04-26 10:44:22 +0200

    Add tests

--- src/main/java/com/example/multitenancy/Application.java
+++ src/main/java/com/example/multitenancy/Application.java
@@ -2,7 +2,6 @@ package com.example.multitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class Application {
--- src/test/java/com/example/multitenancy/configuration/TenantConfigTest.java
+++ src/test/java/com/example/multitenancy/configuration/TenantConfigTest.java
@@ -1,34 +1,21 @@
 package com.example.multitenancy.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class TenantConfigTest {
+
+    @Autowired
+    private TenantConfig tenantConfig;
 
     @Test
     void givenDataSourceMap_whenBindingPropertiesFile_thenDataSourceIsCreated() {
         // Arrange
-        TenantConfig tenantConfig = new TenantConfig();
-        TenantConfig.Tenant tenant = new TenantConfig.Tenant();
-        TenantConfig.DataSource dataSource = new TenantConfig.DataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql:db1");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("password");
-        tenant.setId("tenantId1");
-        tenant.setDataSource(dataSource);
-        List<TenantConfig.Tenant> tenantList = new ArrayList();
-        tenantList.add(tenant);
-        tenantConfig.setTenantList(tenantList);
-
         HikariDataSource expectedDataSource = new HikariDataSource();
         expectedDataSource.setDriverClassName("org.postgresql.Driver");
         expectedDataSource.setJdbcUrl("jdbc:postgresql:db1");
